18806f3de701a5836581f04b38e691eb8ba6b61e
Dianne Cook <visnut@gmail.com>
2016-05-05 16:38:33 +1000

    vis code

--- vis_dirichlet.r
+++ vis_dirichlet.r
@@ -4,7 +4,7 @@ library(tidyr)
 library(ggplot2)
 library(gtools)
 
-raw11 <- read.csv("corrected_data_turk11.csv", stringsAsFactors = FALSE)
+raw11 <- read.csv("data/corrected_data_turk11.csv", stringsAsFactors = FALSE)
 elect <- raw11[grep("elect", as.character(raw11$param_value)),]
 
 response <- strsplit(elect$response_no, split=",")
@@ -49,12 +49,12 @@ ggplot(resp_prop_splx_long, aes(x=response_no, y=p, group=param_value, colour=pa
   geom_line() + scale_colour_hue("Lineup")
 
 # Simulate
-sim <- rdirichlet(10000, rep(1/19, 19))
-sim <- rdirichlet(10000, rep(1, 19))
-sim <- rdirichlet(10000, rep(1/2, 19))
+#sim <- rdirichlet(10000, rep(1/19, 19))
+#sim <- rdirichlet(10000, rep(1, 19))
+#sim <- rdirichlet(10000, rep(1/2, 19))
 sim <- rdirichlet(10000, rep(1/3, 19))
-sim <- rdirichlet(10000, rep(0.1, 19))
-sim <- rdirichlet(10000, rep(1/4, 19))
+#sim <- rdirichlet(10000, rep(0.1, 19))
+#sim <- rdirichlet(10000, rep(1/4, 19))
 for (i in 1:10000)
   sim[i,] <- sort(sim[i,], decreasing = TRUE)
 
@@ -69,4 +69,209 @@ ggplot(sim_df_long, aes(x=response_no, y=p, group=id)) +
   geom_line(alpha=0.1) + 
   geom_line(data=resp_prop_splx_long, 
             aes(x=response_no, y=p, group=param_value, colour=param_value)) +  
+  xlab("Order") + ylab("Proportion") +
+  scale_colour_hue("Lineup")
+
+# Run the autism example
+autism <- raw11[grep("autism", as.character(raw11$param_value)),]
+
+response <- strsplit(autism$response_no, split=",")
+autism$wgt <- response %>% purrr::map(.f = length) %>% unlist()
+bigautism <- autism %>% purrr::map(.f = function(x) rep(x, autism$wgt)) %>% data.frame()
+bigautism$response_no <- unlist(response)
+bigautism$wgt <- 1/bigautism$wgt
+
+# compute proportions for each lineup and all responses
+
+filler <- data.frame(expand.grid(
+  param_value=unique(bigautism$param_value), 
+  response_no=1:20))
+bigautism <- merge(bigautism, filler, by=c("param_value", "response_no"), all=TRUE)
+bigautism$wgt[is.na(bigautism$wgt)] <- 0
+bigautism <- bigautism %>% group_by(param_value) %>% 
+  mutate(plot_location=na.omit(plot_location)[1])
+bigautism$detected <- with(bigautism, !(response_no!=plot_location))
+
+
+nevals <- bigautism %>% filter(detected!=TRUE) %>% 
+  group_by(param_value) %>% summarise(n=sum(wgt, na.rm=T))
+
+bigautismn <- merge(bigautism, nevals)
+
+resp_prop <- bigautismn %>% filter(detected!=TRUE) %>% 
+  mutate(pwgt=wgt/n) %>%
+  group_by(param_value, response_no) %>% 
+  summarise(p=sum(pwgt, na.rm=T))
+
+resp_prop_splx <- resp_prop %>% spread(response_no, p)
+
+# Sort from highest to lowest bc order doesn't matter
+nsets <- nrow(resp_prop_splx)
+for (i in 1:nsets)
+  resp_prop_splx[i,-1] <- sort(resp_prop_splx[i,-1], decreasing = TRUE, na.last = TRUE)
+
+# Plot it
+resp_prop_splx_long <- resp_prop_splx %>% gather(response_no, p, -param_value)
+resp_prop_splx_long$response_no <- rep(1:20,rep(nsets,20))
+
+ggplot(resp_prop_splx_long, aes(x=response_no, y=p, group=param_value, colour=param_value)) + 
+  geom_line() + scale_colour_hue("Lineup")
+
+# Simulate
+sim <- rdirichlet(10000, rep(1/19, 19))
+#sim <- rdirichlet(10000, rep(1, 19))
+#sim <- rdirichlet(10000, rep(1/2, 19))
+sim <- rdirichlet(10000, rep(1/3, 19))
+sim <- rdirichlet(10000, rep(0.1, 19))
+sim <- rdirichlet(10000, rep(1/4, 19))
+sim <- rdirichlet(10000, rep(0.01, 19))
+sim <- rdirichlet(10000, rep(0.05, 19))
+sim <- rdirichlet(10000, rep(0.06, 19))
+for (i in 1:10000)
+  sim[i,] <- sort(sim[i,], decreasing = TRUE)
+
+sim_df <- data.frame(sim)
+sim_df$id <- 1:10000
+sim_df_long <- sim_df %>% gather(response_no, p, -id)
+sim_df_long$response_no <- as.numeric(gsub("X", "", sim_df_long$response_no))
+#ggplot(sim_df_long, aes(x=response_no, y=p, group=id)) + 
+#  geom_line(alpha=0.1)
+
+ggplot(sim_df_long, aes(x=response_no, y=p, group=id)) + 
+  geom_line(alpha=0.1) + 
+  geom_line(data=resp_prop_splx_long, 
+            aes(x=response_no, y=p, group=param_value, colour=param_value)) +  
+  xlab("Order") + ylab("Proportion") +
+  scale_colour_hue("Lineup")
+
+# Run the cyclone example
+cyclone <- raw11[grep("cyclone", as.character(raw11$param_value)),]
+
+response <- strsplit(cyclone$response_no, split=",")
+cyclone$wgt <- response %>% purrr::map(.f = length) %>% unlist()
+bigcyclone <- cyclone %>% purrr::map(.f = function(x) rep(x, cyclone$wgt)) %>% data.frame()
+bigcyclone$response_no <- unlist(response)
+bigcyclone$wgt <- 1/bigcyclone$wgt
+
+# compute proportions for each lineup and all responses
+filler <- data.frame(expand.grid(
+  param_value=unique(bigcyclone$param_value), 
+  response_no=1:20))
+bigcyclone <- merge(bigcyclone, filler, by=c("param_value", "response_no"), all=TRUE)
+bigcyclone$wgt[is.na(bigcyclone$wgt)] <- 0
+bigcyclone <- bigcyclone %>% group_by(param_value) %>% 
+  mutate(plot_location=na.omit(plot_location)[1])
+bigcyclone$detected <- with(bigcyclone, !(response_no!=plot_location))
+
+
+nevals <- bigcyclone %>% filter(detected!=TRUE) %>% 
+  group_by(param_value) %>% summarise(n=sum(wgt, na.rm=T))
+
+bigcyclonen <- merge(bigcyclone, nevals)
+
+resp_prop <- bigcyclonen %>% filter(detected!=TRUE) %>% 
+  mutate(pwgt=wgt/n) %>%
+  group_by(param_value, response_no) %>% 
+  summarise(p=sum(pwgt, na.rm=T))
+
+resp_prop_splx <- resp_prop %>% spread(response_no, p)
+
+# Sort from highest to lowest bc order doesn't matter
+nsets <- nrow(resp_prop_splx)
+for (i in 1:nsets)
+  resp_prop_splx[i,-1] <- sort(resp_prop_splx[i,-1], decreasing = TRUE, na.last = TRUE)
+
+# Plot it
+nchoice <- length(unique(resp_prop_splx_long$response_no))
+resp_prop_splx_long <- resp_prop_splx %>% gather(response_no, p, -param_value)
+resp_prop_splx_long$response_no <- rep(1:nchoice, rep(nsets,nchoice))
+
+ggplot(resp_prop_splx_long, aes(x=response_no, y=p, group=param_value, colour=param_value)) + 
+  geom_line() + scale_colour_hue("Lineup")
+
+# Simulate
+sim <- rdirichlet(10000, rep(1, 19))
+sim <- rdirichlet(10000, rep(2, 19))
+sim <- rdirichlet(10000, rep(0.5, 19))
+for (i in 1:10000)
+  sim[i,] <- sort(sim[i,], decreasing = TRUE)
+
+sim_df <- data.frame(sim)
+sim_df$id <- 1:10000
+sim_df_long <- sim_df %>% gather(response_no, p, -id)
+sim_df_long$response_no <- as.numeric(gsub("X", "", sim_df_long$response_no))
+#ggplot(sim_df_long, aes(x=response_no, y=p, group=id)) + 
+#  geom_line(alpha=0.1)
+
+ggplot(sim_df_long, aes(x=response_no, y=p, group=id)) + 
+  geom_line(alpha=0.1) + 
+  geom_line(data=resp_prop_splx_long, 
+            aes(x=response_no, y=p, group=param_value, colour=param_value)) +  
+  xlab("Order") + ylab("Proportion") +
+  scale_colour_hue("Lineup")
+
+# Run the dialyzerheterogeneous example
+dialyzerheterogeneous <- raw11[grep("dialyzerheterogeneous", as.character(raw11$param_value)),]
+
+response <- strsplit(dialyzerheterogeneous$response_no, split=",")
+dialyzerheterogeneous$wgt <- response %>% purrr::map(.f = length) %>% unlist()
+bigdialyzerheterogeneous <- dialyzerheterogeneous %>% purrr::map(.f = function(x) rep(x, dialyzerheterogeneous$wgt)) %>% data.frame()
+bigdialyzerheterogeneous$response_no <- unlist(response)
+bigdialyzerheterogeneous$wgt <- 1/bigdialyzerheterogeneous$wgt
+
+# compute proportions for each lineup and all responses
+filler <- data.frame(expand.grid(
+  param_value=unique(bigdialyzerheterogeneous$param_value), 
+  response_no=1:20))
+bigdialyzerheterogeneous <- merge(bigdialyzerheterogeneous, filler, by=c("param_value", "response_no"), all=TRUE)
+bigdialyzerheterogeneous$wgt[is.na(bigdialyzerheterogeneous$wgt)] <- 0
+bigdialyzerheterogeneous <- bigdialyzerheterogeneous %>% group_by(param_value) %>% 
+  mutate(plot_location=na.omit(plot_location)[1])
+bigdialyzerheterogeneous$detected <- with(bigdialyzerheterogeneous, !(response_no!=plot_location))
+
+
+nevals <- bigdialyzerheterogeneous %>% filter(detected!=TRUE) %>% 
+  group_by(param_value) %>% summarise(n=sum(wgt, na.rm=T))
+
+bigdialyzerheterogeneousn <- merge(bigdialyzerheterogeneous, nevals)
+
+resp_prop <- bigdialyzerheterogeneousn %>% filter(detected!=TRUE) %>% 
+  mutate(pwgt=wgt/n) %>%
+  group_by(param_value, response_no) %>% 
+  summarise(p=sum(pwgt, na.rm=T))
+
+resp_prop_splx <- resp_prop %>% spread(response_no, p)
+
+# Sort from highest to lowest bc order doesn't matter
+nsets <- nrow(resp_prop_splx)
+for (i in 1:nsets)
+  resp_prop_splx[i,-1] <- sort(resp_prop_splx[i,-1], decreasing = TRUE, na.last = TRUE)
+
+# Plot it
+nchoice <- length(unique(resp_prop_splx_long$response_no))
+resp_prop_splx_long <- resp_prop_splx %>% gather(response_no, p, -param_value)
+resp_prop_splx_long$response_no <- rep(1:nchoice, rep(nsets, nchoice))
+
+ggplot(resp_prop_splx_long, aes(x=response_no, y=p, group=param_value, colour=param_value)) + 
+  geom_line() + scale_colour_hue("Lineup")
+
+# Simulate
+sim <- rdirichlet(10000, rep(1, 19))
+sim <- rdirichlet(10000, rep(2, 19))
+sim <- rdirichlet(10000, rep(0.5, 19))
+for (i in 1:10000)
+  sim[i,] <- sort(sim[i,], decreasing = TRUE)
+
+sim_df <- data.frame(sim)
+sim_df$id <- 1:10000
+sim_df_long <- sim_df %>% gather(response_no, p, -id)
+sim_df_long$response_no <- as.numeric(gsub("X", "", sim_df_long$response_no))
+#ggplot(sim_df_long, aes(x=response_no, y=p, group=id)) + 
+#  geom_line(alpha=0.1)
+
+ggplot(sim_df_long, aes(x=response_no, y=p, group=id)) + 
+  geom_line(alpha=0.1) + 
+  geom_line(data=resp_prop_splx_long, 
+            aes(x=response_no, y=p, group=param_value, colour=param_value)) +  
+  xlab("Order") + ylab("Proportion") +
   scale_colour_hue("Lineup")
